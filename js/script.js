9d6507e25f51d96a152dee5d3b256704f8afb60f
matteo-cota <matteocota@outlook.it>
2024-09-11 17:33:25 +0200

    Inizializzazione di Vue

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Inizializzazione di Vue
+new Vue({
+    el: '#app',
+    data: {
+        emails: [], // Array per memorizzare gli indirizzi email
+        allEmailsGenerated: false // Stato per controllare se tutte le email sono state generate
+    },
+
+});
